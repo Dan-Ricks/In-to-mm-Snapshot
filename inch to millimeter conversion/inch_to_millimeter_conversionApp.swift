9ea691d2472f06ae136fb9603201f459a7bde1e3
Dan Ricks <DanRicks@hotmail.com>
2026-06-20 17:09:35 -0600

    Initial Commit

---
+++ inch to millimeter conversion/inch_to_millimeter_conversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  inch_to_millimeter_conversionApp.swift
+//  inch to millimeter conversion
+//
+//  Created by Dan on 6/20/26.
+//
+
+import SwiftUI
+
+@main
+struct inch_to_millimeter_conversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
